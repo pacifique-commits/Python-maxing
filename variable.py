333f68c4f2ea6abdc53e594714ad598b561bbf39
pacino <typaccy@gmail.com>
2025-08-29 09:30:13 +0200

    Commit

---
+++ variable.py
@@ -0,0 +1,23 @@
+#Variable Assignments
+var1 = "Python"
+var2 = 75 #integer
+var3 = 3.14 # float
+
+print(var1)
+print(var2)
+print(var3)
+
+#Multiple Assignements
+a = b = c = 56
+print(a)
+print(b)
+print(c)
+
+# Multiple Assignnemts with mutiple values
+x, y, z = "Alpha","beta",12
+print("The value of x is:", x)
+print("The value of y is:", y)
+print("The value of z is:", z)
+
+
+
