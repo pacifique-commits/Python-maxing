b3d599db1b97fca57eedcd890ff0ae0364199fc0
pacino <typaccy@gmail.com>
2025-08-29 09:56:55 +0200

    Slicing

---
+++ printing.py
@@ -0,0 +1,9 @@
+name = "Pacifique"
+hobby = "Eating"
+
+print ("My name is ", name + ""
+ " and my hoppy is ", hobby)
+print ("{} loves {}".format(name, hobby))
+print (f"{name} loves {hobby}")
+#concatination
+print ("I will use whatever i remember" )
--- variable.py
+++ variable.py
@@ -14,10 +14,35 @@ print(b)
 print(c)
 
 # Multiple Assignnemts with mutiple values
-x, y, z = "Alpha","beta",12
-print("The value of x is:", x)
-print("The value of y is:", y)
+x, y, z,w = "Alpha","beta",12,3.45
+print("The value of x is:", type(x))
+print("The value of y is:", type(y))
+print("The value of w is:", w)
 print("The value of z is:", z)
 
+#List / collection of multiple datatyples , enclosed in 
+#square brackets
+num1 = 56
+list_str = ["DEVOPS", 7, num1]
+print (list_str)
+
+#Tuple - round bracket enclosed ,- immutable not editable
+# its like a cd drive -read only 
+tup1 = ("King","Queen", 12)
+print (tup1)
+
+#Dictionary -key vlue pairs - curly braces 
+dict1 = {
+    "Name": "Joe",
+    "Age": 34,
+    "City": "New York",
+    "Hobby": "Eating"
+
+}
+print (dict1)
 
 
+# Boolean - True or False 
+x = True
+y = False # used in cndt-nal statment
+print ("The type of x is :", type(x))
