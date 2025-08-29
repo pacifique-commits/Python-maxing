a836703d6a958f93a87feaf21c2366f94f14af70
pacino <typaccy@gmail.com>
2025-08-29 11:24:48 +0200

    commit

---
+++ slicing.py
@@ -0,0 +1,19 @@
+planet1 = "Closest to the sum"
+print (planet1)
+print (planet1[0]) #first letter 
+print (planet1[1]) #second leter
+#Negative indexing
+print (planet1[-1]) #last letter
+print (planet1[-4]) 
+print (planet1[-5])
+
+# Slicing a string
+var = "Hello"
+print (var[0:4]) 
+print("💀")
+print (var[:])
+print (var[:3])
+devops = ("Linux","vagrant","Aws", "python", "docker","Kubernetes")
+# print (devops[0])
+# print (devops[-1])
+print (devops[2:4][0])
