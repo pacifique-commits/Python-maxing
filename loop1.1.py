47fef958f5d94e3d8e274b51c7d1001547f9ac28
pacino <typaccy@gmail.com>
2025-08-30 15:58:41 +0200

    For loopo

---
+++ loop1.1.py
@@ -0,0 +1,5 @@
+PLANET = "Earth"
+for i in PLANET: # itarate over each chracter in the string PLANET
+    print (i)
+print("The rest of the code")
+
---
+++ loop1.py
@@ -0,0 +1,17 @@
+# While Loop 
+""""
+- Reapeat the block of code while condition 
+is true
+- while condition:
+    block of code
+
+"""
+
+# For Loop 
+# Reapeat the Block of code for a number of times
+
+""""
+for variable in sequence 
+    block of code
+
+"""
