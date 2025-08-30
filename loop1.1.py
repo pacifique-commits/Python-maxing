ace6072372850d0329883751b43e58b7b9130c92
pacino <typaccy@gmail.com>
2025-08-30 16:39:17 +0200

    Looping

--- loop1.1.py
+++ loop1.1.py
@@ -1,5 +1,36 @@
-PLANET = "Earth"
-for i in PLANET: # itarate over each chracter in the string PLANET
-    print (i)
-print("The rest of the code")
+# PLANET = "Earth"
+# for i in PLANET: # itarate over each chracter in the string PLANET
+#     print (i)
+# print("The rest of the code")
+
+# Names = ["Peter","John","Sally","Jane"]
+# for name in Names: 
+#     print (name, "Is my Classmate")
+
+
+# While loop
+
+# x = 0
+# while x <= 10:
+#     print ("Value of X is : ", x)
+#     x+=2
+
+
+
+""""
+#Nested for loop
+Names = ["Peter","John","Sally","Jane"]
+for name in Names:
+    print("")
+    print("My Friend is: ")
+    for i in name:
+        print (i)
+"""""
+import time
+x = 2
+while x<= 10:
+  print ("Value of X is :", x)
+  x*=2
+  time.sleep(0.5)
+
 
